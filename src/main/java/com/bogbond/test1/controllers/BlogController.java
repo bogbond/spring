bf057287de8bb91f0b48cffa70bc765164d94530
Bohdan Bondar <43843880+bogbond@users.noreply.github.com>
2022-10-07 10:40:01 +0100

    1

---
+++ src/main/java/com/bogbond/test1/controllers/BlogController.java
@@ -0,0 +1,16 @@
+package com.bogbond.test1.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+
+public class BlogController {
+
+    @GetMapping("/blog")
+    public String blogpage(Model model){
+        return "blog";
+    }
+
+}
